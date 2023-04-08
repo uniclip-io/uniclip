172a36b4258b2be6af0ef9d425f48ecbba89fbf8
noahgreff <greffnoah@gmail.com>
2023-04-08 19:13:23 +0200

    renamed home

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useStore } from './context/store-provider'
-import Devices from './components/views/home/Devices'
+import Dashboard from './components/views/home/Dashboard'
 import Login from './components/views/Login'
 
 export default () => {
 	const { store } = useStore()
 
-	return store.user ? <Devices /> : <Login />
+	return store.user ? <Dashboard /> : <Login />
 }
--- src/renderer/components/views/Login.tsx
+++ src/renderer/components/views/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Login.css'
 // @ts-ignore
-import logo from '../../../assets/logo.png'
+import logo from '../../../../assets/logo.png'
 
 export default () => {
 	return (

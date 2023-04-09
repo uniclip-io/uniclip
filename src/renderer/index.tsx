9f65266e36d69157d168558e80b90623b10d2ab8
noahgreff <greffnoah@gmail.com>
2023-04-09 02:40:22 +0200

    updated assets

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import StoreProvider from './context/store-provider'
 import App from './App'
 
-import 'material-icons/iconfont/round.css'
+import 'material-icons/iconfont/material-icons.css'
 import 'typeface-roboto/index.css'
 
 createRoot(document.getElementById('app')!).render(

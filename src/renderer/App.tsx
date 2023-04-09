cc5e4dcc7416aac2a62600d390d6413a3229b09e
noahgreff <greffnoah@gmail.com>
2023-04-09 19:31:38 +0200

    added loading view

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { useStore } from './context/store-provider'
 import Dashboard from './components/views/home/Dashboard'
 import Login from './components/views/Login'
+import Loading from './components/views/Loading'
+import Header from './components/widgets/Header'
+import './App.css'
 
 export default () => {
 	const { store, isLoading } = useStore()
 
 	if (isLoading) {
-		return <h1>loading...</h1>
+		return <Loading />
 	}
-	return store?.user ? <Dashboard /> : <Login />
+	return (
+		<div className="container">
+			<Header />
+			{store?.user ? <Dashboard /> : <Login />}
+		</div>
+	)
 }
---
+++ src/renderer/components/views/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// @ts-ignore
+import logo from '../../../../assets/logo.png'
+import './Loading.css'
+
+export default () => {
+	return (
+		<div className="loading-container">
+			<img className="loading-logo" src={logo} alt="" />
+			{/* <h1>Uniclip</h1> */}
+		</div>
+	)
+}

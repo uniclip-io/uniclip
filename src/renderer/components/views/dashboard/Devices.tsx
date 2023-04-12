e3656ec80e0eaa6cd9e47067ccc056887543fe28
noahgreff <greffnoah@gmail.com>
2023-04-12 20:40:43 +0200

    imporved login ui

--- src/renderer/components/views/dashboard/Devices.tsx
+++ src/renderer/components/views/dashboard/Devices.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
 
 export default () => {
-	return (
-		<div>
-			<h2>Devices</h2>
-			<button onClick={() => window.electron.logout()}>logout</button>
-		</div>
-	)
+	return <h2>Devices</h2>
 }
